8d1c8f1e1a4327f1f7ff6b4b820384c274a77489
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-12-17 21:17:07 +0100

    development: Add Local Development CA

--- development/certificates/mkcert.sh
+++ development/certificates/mkcert.sh
@@ -2,6 +2,9 @@
 THIS=$(dirname $(readlink -f "$0"))
 NAME=drinks-touch
 
+# until flipdot has a central development certificate
+export CAROOT="$THIS/"
+
 mkcert \
 -cert-file "$THIS/$NAME.crt" \
 -ecdsa \
